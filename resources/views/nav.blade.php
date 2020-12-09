89f0f416d865fce74996e1ea6bcd6bab1fdfdb2f
denliedesign <customdenlie@gmail.com>
2020-12-09 10:57:22 -0600

    customer login nav

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -34,7 +34,11 @@
     <a class="nav-link" href="https://app.jackrabbitclass.com/regv2.asp?id=383292&preLoadClassID=11480783" target="_blank">Register</a>
 </li>
 <li class="nav-item">
+    <a class="nav-link" href="https://app.jackrabbitclass.com/jr3.0/ParentPortal/Login?orgId=383292&regcid=11480783" target="_blank">Login</a>
+</li>
+<li class="nav-item">
     <a class="nav-link" href="https://caledonia-dance-music-center.myshopify.com" target="_blank">Shop</a>
 </li>
+
 
 
