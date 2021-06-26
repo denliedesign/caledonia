21acfd9ea074e620f599d5bc62c32498e00f5a73
denliedesign <customdenlie@gmail.com>
2021-06-26 09:20:41 -0500

    favicon fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,13 +22,6 @@
 
     <title>@yield('title', 'Caledonia Dance & Music Center')</title>
 
-    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
-    <link rel="manifest" href="/favicon/site.webmanifest">
-    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#30393e">
-    <meta name="msapplication-TileColor" content="#30393e">
-    <meta name="theme-color" content="#30393e">
     <meta name="google-site-verification" content="pEj8FQ1at3uraMLV77FgIB99LibvKtfCd2E3Jr8B2Hw" />
 
     <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
