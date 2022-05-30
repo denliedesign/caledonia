f8bb6c7c5c7d05b26e052cf52c053fa1eae321d3
denliedesign <customdenlie@gmail.com>
2022-05-30 12:07:49 -0500

    update testimonials

--- resources/views/testimonials.blade.php
+++ resources/views/testimonials.blade.php
@@ -7,16 +7,16 @@
                         <div class="col-md m-4 p-4 shadow">
                             <div>
 {{--                                <ion-icon name="happy" style="font-size: 2.5em;" class="txt-kapa-orange"></ion-icon>--}}
-                                <img src="/images/ashley.png" alt="ashley" style="height: 40px; width: auto;">
+                                <img src="/images/baker.png" alt="baker" style="height: 40px; width: auto;">
                                 <p class="lead text-center mb-0">
                                     Ashley Baker
                                 </p>
                                 <div class="d-flex justify-content-center text-center" style="color: gold;">
-                                    <ion-icon name="star" style="font-size: 2em;"></ion-icon>
-                                    <ion-icon name="star" style="font-size: 2em;"></ion-icon>
-                                    <ion-icon name="star" style="font-size: 2em;"></ion-icon>
-                                    <ion-icon name="star" style="font-size: 2em;"></ion-icon>
-                                    <ion-icon name="star" style="font-size: 2em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
                                 </div>
                                 <p>
                                     My daughter loves the teachers here. And they are super helpful
@@ -25,10 +25,17 @@
                         </div>
                         <div class="col-md m-4 p-4 shadow">
                             <div>
-                                <ion-icon name="happy" style="font-size: 2.5em;" class="txt-kapa-pink"></ion-icon>
+                                <img src="/images/caldwell.png" alt="caldwell" style="height: 40px; width: auto;">
                                 <p class="lead text-center">
                                     Emily Caldwell
                                 </p>
+                                <div class="d-flex justify-content-center text-center" style="color: gold;">
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                </div>
                                 <p>
                                     My daughter loves CDMC! The teachers are great and the performances are amazing!
                                 </p>
@@ -40,6 +47,13 @@
                                 <p class="lead text-center">
                                     Gretchen Wilson
                                 </p>
+                                <div class="d-flex justify-content-center text-center" style="color: gold;">
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                </div>
                                 <p>
                                     This is a wonderful place with lots of classes to chose from and amazing instructors. They are so organized and know how to put on a show from the Nutcracker at Christmas to Recital week.
                                 </p>
@@ -47,7 +61,7 @@
                         </div>
 {{--                        <div class="col-md m-4 p-4 shadow">--}}
 {{--                            <div>--}}
-{{--                                <ion-icon name="happy" style="font-size: 2.5em;" class="txt-kapa-pink"></ion-icon>--}}
+{{--                                <img src="/images/baker.png" alt="baker" style="height: 40px; width: auto;">--}}
 {{--                                <p class="lead text-center">--}}
 {{--                                    Trevin Steiner--}}
 {{--                                </p>--}}
@@ -83,6 +97,13 @@
                                 <p class="lead text-center">
                                     Angela Russo
                                 </p>
+                                <div class="d-flex justify-content-center text-center" style="color: gold;">
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                </div>
                                 <p>
                                     Both my daughters (3 and 5 years old) attend CDMC and they look forward to going to class every week. All of the CDMC staff are kind, friendly, and knowledgeable. They help build confidence in the dancers and technique all while having fun.
                                 </p>
@@ -90,10 +111,17 @@
                         </div>
                         <div class="col-md m-4 p-4 shadow">
                             <div>
-                                <ion-icon name="happy" style="font-size: 2.5em;" class="txt-kapa-pink"></ion-icon>
+                                <img src="/images/beezhold.png" alt="beezhold" style="height: 40px; width: auto;">
                                 <p class="lead text-center">
                                     Emily Beezhold
                                 </p>
+                                <div class="d-flex justify-content-center text-center" style="color: gold;">
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                </div>
                                 <p>
                                     My daughter has been dancing here since she was four and every year she loves it more and grows in what she knows and her maturity level as well. Even though covid was a tough experience for everything CDMC worked their hardest to continue dance online and in person however they could so the kids continued to be able to learn.
                                 </p>
@@ -105,6 +133,13 @@
                                 <p class="lead text-center">
                                     Stephanie Pratt
                                 </p>
+                                <div class="d-flex justify-content-center text-center" style="color: gold;">
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                </div>
                                 <p>
                                     This is our second year at CDMC and we LOVE it. My daughter danced at two prior studios and CDMC is hands down our favorite and where we will stay. The teachers are knowledgeable and loving, they put on beautiful performances, and we look forward to many years with them. Highly recommend!
                                 </p>
@@ -126,6 +161,13 @@
                                 <p class="lead text-center">
                                     Jennifer French
                                 </p>
+                                <div class="d-flex justify-content-center text-center" style="color: gold;">
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                </div>
                                 <p>
                                     LOVE this studio! High quality instruction and great teachers and staff! No better dance studio in this area!! We love CDMC!!
                                 </p>
@@ -133,10 +175,17 @@
                         </div>
                         <div class="col-md m-4 p-4 shadow">
                             <div>
-                                <ion-icon name="happy" style="font-size: 2.5em;" class="txt-kapa-pink"></ion-icon>
+                                <img src="/images/dallas.png" alt="dallas" style="height: 40px; width: auto;">
                                 <p class="lead text-center">
                                     Melanie Dallas
                                 </p>
+                                <div class="d-flex justify-content-center text-center" style="color: gold;">
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                </div>
                                 <p>
                                     Great teachers and staff members!!! Cdmc makes u feel like family. My daughter has been dancing for the last 4 years at cdmc and loves ever minute it.
                                 </p>
@@ -148,6 +197,13 @@
                                 <p class="lead text-center">
                                     Joanna Shear
                                 </p>
+                                <div class="d-flex justify-content-center text-center" style="color: gold;">
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                </div>
                                 <p>
                                     My daughter adores her teacher and the office staff is very helpful and friendly. We've loved our experience so far.
                                 </p>
@@ -167,6 +223,13 @@
                                 <p class="lead text-center">
                                     Ashley Nadeau
                                 </p>
+                                <div class="d-flex justify-content-center text-center" style="color: gold;">
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                </div>
                                 <p>
                                     Our three year old loves coming to ballet lessons! Very nice teachers/staff!
                                 </p>
@@ -174,7 +237,7 @@
                         </div>
 {{--                        <div class="col-md m-4 p-4 shadow">--}}
 {{--                            <div>--}}
-{{--                                <ion-icon name="happy" style="font-size: 2.5em;" class="txt-kapa-pink"></ion-icon>--}}
+{{--                                <img src="/images/baker.png" alt="baker" style="height: 40px; width: auto;">--}}
 {{--                                <p class="lead text-center">--}}
 {{--                                    Ashley--}}
 {{--                                </p>--}}
@@ -185,10 +248,17 @@
 {{--                        </div>--}}
                         <div class="col-md m-4 p-4 shadow">
                             <div>
-                                <ion-icon name="happy" style="font-size: 2.5em;" class="txt-kapa-pink"></ion-icon>
+                                <img src="/images/adams.png" alt="adams" style="height: 40px; width: auto;">
                                 <p class="lead text-center">
                                     Megan Adams
                                 </p>
+                                <div class="d-flex justify-content-center text-center" style="color: gold;">
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                </div>
                                 <p>
                                     I love CDMC. We have been here 6 years now and we have loved every minute of it. They keep parents in the loop and the kids are cared for and taken care of very well. We love our dance family!
                                 </p>
@@ -200,6 +270,13 @@
                                 <p class="lead text-center">
                                     Patrick Kane
                                 </p>
+                                <div class="d-flex justify-content-center text-center" style="color: gold;">
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                    <ion-icon name="star" style="font-size: 1.75em;"></ion-icon>
+                                </div>
                                 <p>
                                     Very talented staff!
                                 </p>
