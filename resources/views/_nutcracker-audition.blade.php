dc02acb29f3742a9d2ec1f7e5f96d457bd519ffc
denliedesign <customdenlie@gmail.com>
2023-01-06 15:26:18 -0600

    nutcracker auditions

---
+++ resources/views/_nutcracker-audition.blade.php
@@ -0,0 +1,46 @@
+<div class="container">
+    <div class="d-flex justify-content-center py-4">
+        <img src="/images/nutcracker-logo.jpg" alt="nutcracker logo" class="img-fluid">
+    </div>
+{{--    <div class="my-4">--}}
+{{--        <h2 class="text-center">Nutcracker 2023</h2>--}}
+{{--    </div>--}}
+{{--    <div class="d-flex justify-content-center pb-4">--}}
+{{--        <img src="/images/nutcracker-2022b.jpg" alt="nutcracker performance flyer" class="img-fluid">--}}
+{{--    </div>--}}
+</div>
+
+
+
+       <div class="container">
+           <h2 class="text-center">Nutcracker Auditions 2023</h2>
+{{--           <p class="lead text-center">Auditions will be held on Saturday, October 1, 2022</p>--}}
+           <div id="class-schedule">
+               <script type="text/javascript" src="https://app.jackrabbitclass.com/jr3.0/Openings/OpeningsJS?OrgID=383292&Session=Nutcracker"></script>
+           </div>
+       </div>
+
+<!-- /////////////////////////// -->
+<!-- GALLERY -->
+<!-- /////////////////////////// -->
+
+<div class="background-green py-3">
+    <div class="container">
+        <div class="">
+            <div class="row p-0 m-0 d-flex justify-content-center align-items-center">
+                <div class="col-sm my-1 d-flex justify-content-center"><img src="/images/nutcracker-1.jpg" style="width: 263px; height: 200px; object-fit: cover; object-position: 50% 0%;" class="rounded shadow"></div>
+                <div class="col-sm my-1 d-flex justify-content-center"><img src="/images/nutcracker-2.jpg" style="width: 263px; height: 200px; object-fit: cover; object-position: 50% 0%;" class="rounded shadow"></div>
+                <div class="col-sm my-1 d-flex justify-content-center"><img src="/images/nutcracker-4.jpg" style="width: 263px; height: 200px; object-fit: cover; object-position: 50% 0%;" class="rounded shadow"></div>
+            </div>
+            <div class="row p-0 m-0 d-flex justify-content-center align-items-center">
+                <div class="col-sm my-1 d-flex justify-content-center"><img src="/images/nutcracker-5.jpg" style="width: 263px; height: 200px; object-fit: cover; object-position: 50% 0%;" class="rounded shadow"></div>
+                <div class="col-sm my-1 d-flex justify-content-center"><img src="/images/nutcracker-2021a.jpg" style="width: 263px; height: 200px; object-fit: cover; object-position: 50% 0%;" class="rounded shadow"></div>
+                <div class="col-sm my-1 d-flex justify-content-center"><img src="/images/nutcracker-2021b.jpeg" style="width: 263px; height: 200px; object-fit: cover; object-position: 50% 0%;" class="rounded shadow"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- /////////////////////////// -->
+<!-- END GALLERY -->
+<!-- /////////////////////////// -->
