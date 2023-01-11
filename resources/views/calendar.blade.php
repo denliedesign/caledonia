58f54824a34c8368fab10ba62dc7e758e6210131
denliedesign <customdenlie@gmail.com>
2023-01-11 10:16:50 -0600

    cal update

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -8,7 +8,7 @@
         </div>
 
         <div class="container py-5">
-            <h2 class="text-center mt-3">2022-2023 Calendar</h2>
+            <h2 class="text-center mt-3">2023 Calendar</h2>
             <div class="d-flex justify-content-center">
                 {{--            <img src="/images/cdmc-calendar.png" alt="calendar" class="img-fluid">--}}
                 <table>
@@ -57,51 +57,51 @@
 {{--                        <td>&nbsp;|&nbsp;</td>--}}
 {{--                        <td>Music Recital Performance</td>--}}
 {{--                    </tr>--}}
-                    <tr>
-                        <td class="text-right"><strong>Aug 29</strong></td>
-                        <td>&nbsp;|&nbsp;</td>
-                        <td>First Day of Dance for the 2022-2023 Season</td>
-                    </tr>
-                    <tr>
-                        <td class="text-right"><strong>Sept 5</strong></td>
-                        <td>&nbsp;|&nbsp;</td>
-                        <td>Obsv. Labor Day - Closed</td>
-                    </tr>
-                    <tr>
-                        <td class="text-right"><strong>Oct 1</strong></td>
-                        <td>&nbsp;|&nbsp;</td>
-                        <td>Nutcracker Auditions</td>
-                    </tr>
-                    <tr>
-                        <td class="text-right"><strong>Oct 17-21</strong></td>
-                        <td>&nbsp;|&nbsp;</td>
-                        <td>Inter-sessional Break #1</td>
-                    </tr>
-                    <tr>
-                        <td class="text-right"><strong>Oct 31</strong></td>
-                        <td>&nbsp;|&nbsp;</td>
-                        <td>Obsv. Halloween - Closed</td>
-                    </tr>
-                    <tr>
-                        <td class="text-right"><strong>Nov 22-25</strong></td>
-                        <td>&nbsp;|&nbsp;</td>
-                        <td>Thanksgiving Break</td>
-                    </tr>
-                    <tr>
-                        <td class="text-right"><strong>Dec 8</strong></td>
-                        <td>&nbsp;|&nbsp;</td>
-                        <td>Nutcracker Rehearsals - No Class</td>
-                    </tr>
-                    <tr>
-                        <td class="text-right"><strong>Dec 10-11</strong></td>
-                        <td>&nbsp;|&nbsp;</td>
-                        <td>Nutcracker Performances</td>
-                    </tr>
-                    <tr>
-                        <td class="text-right"><strong>Dec 19-Jan 2</strong></td>
-                        <td>&nbsp;|&nbsp;</td>
-                        <td>Christmas Break</td>
-                    </tr>
+{{--                    <tr>--}}
+{{--                        <td class="text-right"><strong>Aug 29</strong></td>--}}
+{{--                        <td>&nbsp;|&nbsp;</td>--}}
+{{--                        <td>First Day of Dance for the 2022-2023 Season</td>--}}
+{{--                    </tr>--}}
+{{--                    <tr>--}}
+{{--                        <td class="text-right"><strong>Sept 5</strong></td>--}}
+{{--                        <td>&nbsp;|&nbsp;</td>--}}
+{{--                        <td>Obsv. Labor Day - Closed</td>--}}
+{{--                    </tr>--}}
+{{--                    <tr>--}}
+{{--                        <td class="text-right"><strong>Oct 1</strong></td>--}}
+{{--                        <td>&nbsp;|&nbsp;</td>--}}
+{{--                        <td>Nutcracker Auditions</td>--}}
+{{--                    </tr>--}}
+{{--                    <tr>--}}
+{{--                        <td class="text-right"><strong>Oct 17-21</strong></td>--}}
+{{--                        <td>&nbsp;|&nbsp;</td>--}}
+{{--                        <td>Inter-sessional Break #1</td>--}}
+{{--                    </tr>--}}
+{{--                    <tr>--}}
+{{--                        <td class="text-right"><strong>Oct 31</strong></td>--}}
+{{--                        <td>&nbsp;|&nbsp;</td>--}}
+{{--                        <td>Obsv. Halloween - Closed</td>--}}
+{{--                    </tr>--}}
+{{--                    <tr>--}}
+{{--                        <td class="text-right"><strong>Nov 22-25</strong></td>--}}
+{{--                        <td>&nbsp;|&nbsp;</td>--}}
+{{--                        <td>Thanksgiving Break</td>--}}
+{{--                    </tr>--}}
+{{--                    <tr>--}}
+{{--                        <td class="text-right"><strong>Dec 8</strong></td>--}}
+{{--                        <td>&nbsp;|&nbsp;</td>--}}
+{{--                        <td>Nutcracker Rehearsals - No Class</td>--}}
+{{--                    </tr>--}}
+{{--                    <tr>--}}
+{{--                        <td class="text-right"><strong>Dec 10-11</strong></td>--}}
+{{--                        <td>&nbsp;|&nbsp;</td>--}}
+{{--                        <td>Nutcracker Performances</td>--}}
+{{--                    </tr>--}}
+{{--                    <tr>--}}
+{{--                        <td class="text-right"><strong>Dec 19-Jan 2</strong></td>--}}
+{{--                        <td>&nbsp;|&nbsp;</td>--}}
+{{--                        <td>Christmas Break</td>--}}
+{{--                    </tr>--}}
                     <tr>
                         <td class="text-right"><strong>Jan 16</strong></td>
                         <td>&nbsp;|&nbsp;</td>
@@ -132,15 +132,35 @@
                         <td>&nbsp;|&nbsp;</td>
                         <td>Obsv. Memorial Day - Closed</td>
                     </tr>
+{{--                    <tr>--}}
+{{--                        <td class="text-right"><strong>June 5-9</strong></td>--}}
+{{--                        <td>&nbsp;|&nbsp;</td>--}}
+{{--                        <td>Proposed Dance Dress Rehearsals</td>--}}
+{{--                    </tr>--}}
+{{--                    <tr>--}}
+{{--                        <td class="text-right"><strong>June 12-16</strong></td>--}}
+{{--                        <td>&nbsp;|&nbsp;</td>--}}
+{{--                        <td>Proposed Dance Recitals</td>--}}
+{{--                    </tr>--}}
                     <tr>
-                        <td class="text-right"><strong>June 5-9</strong></td>
+                        <td class="text-right"><strong>June 5-8</strong></td>
                         <td>&nbsp;|&nbsp;</td>
-                        <td>Proposed Dance Dress Rehearsals</td>
+                        <td>Dance Dress Rehearsals</td>
                     </tr>
                     <tr>
-                        <td class="text-right"><strong>June 12-16</strong></td>
+                        <td class="text-right"><strong>June 12-15</strong></td>
                         <td>&nbsp;|&nbsp;</td>
-                        <td>Proposed Dance Recitals</td>
+                        <td>Dance Recitals</td>
+                    </tr>
+                    <tr>
+                        <td class="text-right"><strong>June 19</strong></td>
+                        <td>&nbsp;|&nbsp;</td>
+                        <td>Music Dress Rehearsal</td>
+                    </tr>
+                    <tr>
+                        <td class="text-right"><strong>June 20</strong></td>
+                        <td>&nbsp;|&nbsp;</td>
+                        <td>Music Recital</td>
                     </tr>
                 </table>
             </div>
