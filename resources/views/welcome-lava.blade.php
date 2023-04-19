14986e9b7d37d83449ede4157ac3512221980475
denliedesign <customdenlie@gmail.com>
2023-04-19 12:03:20 -0500

    sub txt for box

--- resources/views/welcome-lava.blade.php
+++ resources/views/welcome-lava.blade.php
@@ -14,7 +14,7 @@
     <div id="summer-stick" class="d-flex align-items-center justify-content-center shadow d-none d-lg-flex">
         <div style="position:relative; z-index: 6;">
             <p class="text-center summer-available pb-0">JOIN US THIS SUMMER
-{{--                <br><small><span style="font-weight: normal;">classes for kids & adults</span></small>--}}
+                <br><small><span style="font-weight: normal;">music & dance classes for all ages</span></small>
             </p>
             {{--            <p class="text-center summer-available">SUMMER SCHEDULE NOW AVAILABLE</p>--}}
             <div class="d-flex justify-content-center">
