1267d23a6182377aa6a9d5517ecb7a4a841438cf
denliedesign <customdenlie@gmail.com>
2023-12-11 10:32:45 -0600

    ballerinas in training nutcracker page

--- resources/views/_nutcracker-audition.blade.php
+++ resources/views/_nutcracker-audition.blade.php
@@ -2,13 +2,15 @@
     <div class="d-flex justify-content-center py-4">
         <img src="/images/nutcracker-logo.jpg" alt="nutcracker logo" class="img-fluid">
     </div>
-    <p class="text-center my-3" style="font-size: 1.5em;">CDMC Presents our 15th Annual Performance of The Nutcracker, the classic story of a young girl named Clara and a fascinating toy Nutcracker gifted to her by her enchanting godfather Dr. Drosselmeyer.  We invite you to experience the magic this holiday season.  Three performances of The Nutcracker will take place December 9th and 10th at Caledonia’s Duncan Lake Middle School Performing Arts Center.</p>
+{{--    <p class="text-center my-3" style="font-size: 1.5em;">CDMC Presents our 15th Annual Performance of The Nutcracker, the classic story of a young girl named Clara and a fascinating toy Nutcracker gifted to her by her enchanting godfather Dr. Drosselmeyer.  We invite you to experience the magic this holiday season.  Three performances of The Nutcracker will take place December 9th and 10th at Caledonia’s Duncan Lake Middle School Performing Arts Center.</p>--}}
+    <p class="text-center my-3" style="font-size: 1.5em;">A heartfelt thank you to all attendees for making our 15th Annual Nutcracker truly spectacular. Congratulations to our exceptional performers whose talent and dedication brought the magic to life. Mark your calendars for Nutcracker 2024 on Dec 14 and 15 – we can't wait to enchant you again!</p>
 {{--    <p class="text-center mb-1"><small>Tickets go on sale Saturday, October 28th at Noon</small></p>--}}
     <div class="d-flex justify-content-center mb-5">
-        <a href="https://25664.recitalticketing.com/landing.cfm?loc=4" target="_blank"><div class="btn btn-lg btn-danger fw-bold shadow">Get Tickets Now</div></a>
+{{--        <a href="https://25664.recitalticketing.com/landing.cfm?loc=4" target="_blank"><div class="btn btn-lg btn-danger fw-bold shadow">Get Tickets Now</div></a>--}}
     </div>
     <div class="d-flex justify-content-center">
-        <img src="/images/nutcracker-caledonia-2023.jpg" alt="nutcracker performance flyer" class="img-fluid">
+{{--        <img src="/images/nutcracker-caledonia-2023.jpg" alt="nutcracker performance flyer" class="img-fluid">--}}
+        <img src="/images/ballerinas-in-training.jpg" alt="nutcracker performance flyer" class="img-fluid">
     </div>
     <script type="text/javascript" src="https://app.jackrabbitclass.com/jr3.0/Openings/OpeningsJS?OrgID=383292&Session=Nutcracker"></script>
 </div>
