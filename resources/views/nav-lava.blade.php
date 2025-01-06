1d66d9af6f5640dc33132aa0379d1ef69cb7d607
denliedesign <customdenlie@gmail.com>
2025-01-06 10:11:41 -0600

    remove nutcracker

--- resources/views/nav-lava.blade.php
+++ resources/views/nav-lava.blade.php
@@ -34,11 +34,11 @@
 {{--                <li class="nav-item mx-2">--}}
 {{--                    <a class="nav-link" href="/recital">Recital</a>--}}
 {{--                </li>--}}
-                <li class="nav-item mx-2">
-                    <a class="nav-link" href="/nutcracker">Nutcracker
+{{--                <li class="nav-item mx-2">--}}
+{{--                    <a class="nav-link" href="/nutcracker">Nutcracker--}}
 {{--                                    <br><span class="text-muted" style="font-size: 0.65em;">TICKETS AVAILABLE</span>--}}
-                    </a>
-                </li>
+{{--                    </a>--}}
+{{--                </li>--}}
                 <li class="nav-item mx-2">
                     <a class="nav-link" href="/calendar">Calendar</a>
                 </li>
