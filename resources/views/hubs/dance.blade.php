5a59102fcf740c1824c5cfa6eb21cfb1575c05b2
denliedesign <customdenlie@gmail.com>
2025-04-08 08:22:48 -0500

    finishing touches recital hub

--- resources/views/hubs/dance.blade.php
+++ resources/views/hubs/dance.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
     <div class="banner-wrap d-none d-md-block" style="position: relative;">
-        <div class="banner-hub-25"></div>
+        <div class="banner-hub"></div>
         <div class="custom-shape-divider-bottom-1663856745">
             <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                 <path d="M598.97 114.72L0 0 0 120 1200 120 1200 0 598.97 114.72z" class="shape-fill"></path>
@@ -23,12 +23,14 @@
                 <div class="col"><a href="/images/recital-4.png"><img src="/images/recital/dance-recital-4.png" alt="" class="img-fluid"></a></div>
                 <div class="col"><a href="/images/ticket-faq.png"><img src="/images/recital/recital-ticket-faqs.png" alt="" class="img-fluid"></a></div>
                 <div class="col"><a href="/images/stage-ready.png"><img src="/images/recital/how-to-be-stage-ready.png" alt="" class="img-fluid"></a></div>
-{{--                <div class="col"><a href="/images/recital-lookbook.pdf"><img src="/images/recital/recital-lookbook.png" alt="" class="img-fluid"></a></div>--}}
-{{--                <div class="col"><a target="_blank" href="ttps://forms.gle/twyWPqURosGor9Jx9"><img src="/images/recital/recital-prep-shop-link.png" alt="" class="img-fluid"></a></div>--}}
+                <div class="col"><a href="/images/coming-soon.png"><img src="/images/recital/recital-lookbook.png" alt="" class="img-fluid"></a></div>
+                <div class="col"><a target="_blank" href="/images/coming-soon.png"><img src="/images/recital/recital-prep-shop-link.png" alt="" class="img-fluid"></a></div>
                 <div class="col"><a href="/images/recital-faq.pdf"><img src="/images/recital/recital-day-faqs.png" alt="" class="img-fluid"></a></div>
                 <div class="col"><a target="_blank" href="/images/coming-soon.png"><img src="/images/recital/recital-swag.png" alt="" class="img-fluid"></a></div>
                 <div class="col"><a target="_blank" href="https://www.youtube.com/playlist?app=desktop&list=PL6ugHv6O3tUoo8B5JUxI4yyhIMLjvnL2Q"><img src="/images/recital/how-to-do-hair-and-makeup.png" alt="" class="img-fluid"></a></div>
                 <div class="col"><a target="_blank" href="https://docs.google.com/spreadsheets/d/e/2PACX-1vQcBj0dOFiDG0zCloSvVQBq2dLEvLdQV0YWOrfR2cHyOWnKPdCvD5olC3rNl9H9BWiFiISKL-Is3h7B/pubhtml?gid=0&single=true"><img src="/images/recital/picture-schedule.png" alt="" class="img-fluid"></a></div>
+                <div class="col"><a target="_blank" href="https://25664.recitalticketing.com/landing.cfm"><img src="/images/recital/recital-tickets.png" alt="" class="img-fluid"></a></div>
+
             </div>
 
             <div class="mt-5">
