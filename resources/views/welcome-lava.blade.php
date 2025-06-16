b1d41cd87ae4a11cfb35e2d9b282b9e702e9c11c
denliedesign <customdenlie@gmail.com>
2025-06-16 11:58:53 -0500

    remove recital popup

--- resources/views/welcome-lava.blade.php
+++ resources/views/welcome-lava.blade.php
@@ -1,28 +1,28 @@
 @extends('layouts.app-lava')
 @section('content')
 
-        <div>
-            <div style="background: #3D1951; width: 300px; height: 300px; top: 300px;" id="side-trial" class="offcanvas offcanvas-end show shadow" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
-                <div class="offcanvas-header p-0 mt-2" style="position: relative;">
-                    <div class="d-flex justify-content-center" style="width: 100%;">
-                        <h2 class="text-center font-staat-side text-white" style="font-size: 1.5em;">Recital 2025</h2>
-                    </div>
-                    <div style="position: absolute; top: 10px; right: 0; z-index: 4;">
-                        <button class="mx-2" type="button" data-bs-dismiss="offcanvas" aria-label="Close" style="background: transparent; border: none;">
-                            <ion-icon name="close-outline" style="color: white; font-size: 2em;"></ion-icon>
-                        </button>
-                    </div>
-                </div>
-                <div class="offcanvas-body py-0">
-                    <div class="d-flex justify-content-center">
-                        <img src="/images/recital/recital-tickets.png" alt="ballerinas in training logo" class="img-fluid rounded" style="height: 175px; width: auto;">
-                    </div>
-                    <div class="d-flex justify-content-center">
-                        <a href="https://25664.recitalticketing.com/landing.cfm" target="_blank"><div class="m-2 btn btn-danger fw-bold shadow">Get Tickets Now</div></a>
-                    </div>
-                </div>
-            </div>
-        </div>
+{{--        <div>--}}
+{{--            <div style="background: #3D1951; width: 300px; height: 300px; top: 300px;" id="side-trial" class="offcanvas offcanvas-end show shadow" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">--}}
+{{--                <div class="offcanvas-header p-0 mt-2" style="position: relative;">--}}
+{{--                    <div class="d-flex justify-content-center" style="width: 100%;">--}}
+{{--                        <h2 class="text-center font-staat-side text-white" style="font-size: 1.5em;">Recital 2025</h2>--}}
+{{--                    </div>--}}
+{{--                    <div style="position: absolute; top: 10px; right: 0; z-index: 4;">--}}
+{{--                        <button class="mx-2" type="button" data-bs-dismiss="offcanvas" aria-label="Close" style="background: transparent; border: none;">--}}
+{{--                            <ion-icon name="close-outline" style="color: white; font-size: 2em;"></ion-icon>--}}
+{{--                        </button>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="offcanvas-body py-0">--}}
+{{--                    <div class="d-flex justify-content-center">--}}
+{{--                        <img src="/images/recital/recital-tickets.png" alt="ballerinas in training logo" class="img-fluid rounded" style="height: 175px; width: auto;">--}}
+{{--                    </div>--}}
+{{--                    <div class="d-flex justify-content-center">--}}
+{{--                        <a href="https://25664.recitalticketing.com/landing.cfm" target="_blank"><div class="m-2 btn btn-danger fw-bold shadow">Get Tickets Now</div></a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
 
     <div>
         <div style="background: white; width: 300px; height: 300px; top: 0px;" id="side-trial" class="offcanvas offcanvas-end show shadow" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
