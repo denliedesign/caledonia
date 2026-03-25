0f36e01e4f80439ef7d48be80039f56d8d272bf6
denliedesign <customdenlie@gmail.com>
2026-03-24 22:45:33 -0500

    uniform graphic

--- resources/views/dress-code-lava.blade.php
+++ resources/views/dress-code-lava.blade.php
@@ -56,108 +56,114 @@
 {{--                <p class="font-syne-side">--}}
 {{--                    <a href="https://forms.gle/E1zMNM6EqUUxJNK29" target="_blank">CDMC Online Merchandise Order Form</a>--}}
 {{--                </p>--}}
-                <div id="uniform">
-                    <h4 class="text-center font-staat-side">Female Students</h4>
-                    <div class="row font-syne-side">
-                        <div class="col-sm d-flex justify-content-end">
-                            <img src="/images/uniform-1.png" alt="" class="img-fluid" style="max-height: 500px;">
-                        </div>
-                        <div class="col-sm d-flex align-items-center">
-                            <div>
-                                <strong>Ages 2-4</strong>
-                                <dl>
-                                    <dt class="fw-normal">- Pink leotard/dress</dt>
-                                    <dt class="fw-normal">- Pink tights</dt>
-                                    <dt class="fw-normal">- Appropriate Class Shoes</dt>
-                                    <dd>-   <u>Ballet:</u> Pink Revolution Full Sole Ballet Shoes</dd>
-                                    <dd>-   <u>Acro:</u> Pink Revolution Full Sole Ballet Shoes</dd>
-                                    <dd>-   <u>Tap:</u> Tan Ushell Tap Shoes</dd>
-                                </dl>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row font-syne-side">
-                        <div class="col-sm d-flex justify-content-end">
-                            <img src="/images/purple-dancer-2.png" alt="" class="img-fluid" style="max-height: 500px;">
-                        </div>
-                        <div class="col-sm d-flex align-items-center">
-                            <div>
-                                <strong>K-1st Grade</strong>
-                                <dl>
-                                    <dt class="fw-normal">- Lilac leotard/dress</dt>
-                                    <dt class="fw-normal">- Pink tights</dt>
-                                    <dt class="fw-normal">- Appropriate Class Shoes</dt>
-                                    <dd>-   <u>Acro:</u> Bare feet</dd>
-                                    <dd>-   <u>Hip Hop:</u> Icon Dance Sneaker</dd>
-                                    <dd>-   <u>Tap:</u> Tan Ushell Tap Shoes</dd>
-                                    <dd>-   <u>Ballet:</u> Pink Revolution Stretch Ballet Shoes</dd>
-                                </dl>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row font-syne-side">
-                        <div class="col-sm d-flex justify-content-end">
-                            <img src="/images/uniform-3.png" alt="" class="img-fluid" style="max-height: 500px;">
-                        </div>
-                        <div class="col-sm d-flex align-items-center">
-                            <div>
-                                <strong>2nd-3rd Grade</strong>
-                                <dl>
-                                    <dt class="fw-normal">- Blue leotard/dress</dt>
-                                    <dt class="fw-normal">- Pink tights</dt>
-                                    <dt class="fw-normal">- Appropriate Class Shoes</dt>
-                                    <dd>-   <u>Acro:</u> Bare feet</dd>
-                                    <dd>-   <u>Hip Hop:</u> Icon Dance Sneaker</dd>
-                                    <dd>-   <u>Tap:</u> Tan Ushell Tap Shoes</dd>
-                                    <dd>-   <u>Ballet:</u> Pink Revolution Stretch Ballet Shoes</dd>
-                                    <dd>-   <u>Jazz/Lyrical:</u> Tan Bloch Super Jazz Shoes</dd>
-                                </dl>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row font-syne-side">
-                        <div class="col-sm d-flex justify-content-end">
-                            <img src="/images/apparel-4th-grade-plus.png" alt="" class="img-fluid" style="max-height: 500px;">
-                        </div>
-                        <div class="col-sm d-flex align-items-center">
-                            <div>
-                                <strong>4th+ Grade</strong>
-                                <dl>
-                                    <dt class="fw-normal">- Black leotard/dress</dt>
-                                    <dt class="fw-normal">- Pink tights</dt>
-                                    <dt class="fw-normal">- Appropriate Class Shoes</dt>
-                                    <dd>-   <u>Acro:</u> Bare feet</dd>
-                                    <dd>-   <u>Hip Hop:</u> Icon Dance Sneaker</dd>
-                                    <dd>-   <u>Tap:</u> Black Capezio Tap Shoes</dd>
-                                    <dd>-   <u>Ballet:</u> Pink Revolution Stretch Ballet Shoes</dd>
-                                    <dd>-   <u>Jazz/Lyrical:</u> Tan Bloch Super Jazz Shoes</dd>
-                                </dl>
-                            </div>
-                        </div>
-                    </div>
-                    <h4 class="text-center font-staat-side">Male Students</h4>
-                    <div class="row font-syne-side">
-                        <div class="col-sm d-flex justify-content-end">
-                            <img src="/images/uniform-5.png" alt="" class="img-fluid" style="max-height: 500px;">
-                        </div>
-                        <div class="col-sm d-flex align-items-center">
-                            <div>
-                                <dl>
-                                    <dt class="fw-normal">- White tank top</dt>
-                                    <dt class="fw-normal">- Black joggers</dt>
-                                    <dt class="fw-normal">- Appropriate Class Shoes</dt>
-                                    <dd>-   <u>Acro:</u> Bare feet</dd>
-                                    <dd>-   <u>Hip Hop:</u> Icon Dance Sneaker</dd>
-                                    <dd>-   <u>Ballet:</u> White Revolution Stretch Ballet Shoes</dd>
-                                    <dd>-   <u>Jazz:</u> Black Bloch Super Jazz Shoes</dd>
-                                    <dd>-   <u>Tap:</u></dd>
-                                    <dd style="padding-left: 15px;">&middot;    Ages 2-4 - Black Revolution Oxford Tap Shoes</dd>
-                                    <dd style="padding-left: 15px;">&middot;    Kindergarten and Up - Black Capezio Oxford Tap Shoes</dd>
-                                </dl>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
+
+{{--                <div id="uniform">--}}
+{{--                    <h4 class="text-center font-staat-side">Female Students</h4>--}}
+{{--                    <div class="row font-syne-side">--}}
+{{--                        <div class="col-sm d-flex justify-content-end">--}}
+{{--                            <img src="/images/uniform-1.png" alt="" class="img-fluid" style="max-height: 500px;">--}}
+{{--                        </div>--}}
+{{--                        <div class="col-sm d-flex align-items-center">--}}
+{{--                            <div>--}}
+{{--                                <strong>Ages 2-4</strong>--}}
+{{--                                <dl>--}}
+{{--                                    <dt class="fw-normal">- Pink leotard/dress</dt>--}}
+{{--                                    <dt class="fw-normal">- Pink tights</dt>--}}
+{{--                                    <dt class="fw-normal">- Appropriate Class Shoes</dt>--}}
+{{--                                    <dd>-   <u>Ballet:</u> Pink Revolution Full Sole Ballet Shoes</dd>--}}
+{{--                                    <dd>-   <u>Acro:</u> Pink Revolution Full Sole Ballet Shoes</dd>--}}
+{{--                                    <dd>-   <u>Tap:</u> Tan Ushell Tap Shoes</dd>--}}
+{{--                                </dl>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                    <div class="row font-syne-side">--}}
+{{--                        <div class="col-sm d-flex justify-content-end">--}}
+{{--                            <img src="/images/purple-dancer-2.png" alt="" class="img-fluid" style="max-height: 500px;">--}}
+{{--                        </div>--}}
+{{--                        <div class="col-sm d-flex align-items-center">--}}
+{{--                            <div>--}}
+{{--                                <strong>K-1st Grade</strong>--}}
+{{--                                <dl>--}}
+{{--                                    <dt class="fw-normal">- Lilac leotard/dress</dt>--}}
+{{--                                    <dt class="fw-normal">- Pink tights</dt>--}}
+{{--                                    <dt class="fw-normal">- Appropriate Class Shoes</dt>--}}
+{{--                                    <dd>-   <u>Acro:</u> Bare feet</dd>--}}
+{{--                                    <dd>-   <u>Hip Hop:</u> Icon Dance Sneaker</dd>--}}
+{{--                                    <dd>-   <u>Tap:</u> Tan Ushell Tap Shoes</dd>--}}
+{{--                                    <dd>-   <u>Ballet:</u> Pink Revolution Stretch Ballet Shoes</dd>--}}
+{{--                                </dl>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                    <div class="row font-syne-side">--}}
+{{--                        <div class="col-sm d-flex justify-content-end">--}}
+{{--                            <img src="/images/uniform-3.png" alt="" class="img-fluid" style="max-height: 500px;">--}}
+{{--                        </div>--}}
+{{--                        <div class="col-sm d-flex align-items-center">--}}
+{{--                            <div>--}}
+{{--                                <strong>2nd-3rd Grade</strong>--}}
+{{--                                <dl>--}}
+{{--                                    <dt class="fw-normal">- Blue leotard/dress</dt>--}}
+{{--                                    <dt class="fw-normal">- Pink tights</dt>--}}
+{{--                                    <dt class="fw-normal">- Appropriate Class Shoes</dt>--}}
+{{--                                    <dd>-   <u>Acro:</u> Bare feet</dd>--}}
+{{--                                    <dd>-   <u>Hip Hop:</u> Icon Dance Sneaker</dd>--}}
+{{--                                    <dd>-   <u>Tap:</u> Tan Ushell Tap Shoes</dd>--}}
+{{--                                    <dd>-   <u>Ballet:</u> Pink Revolution Stretch Ballet Shoes</dd>--}}
+{{--                                    <dd>-   <u>Jazz/Lyrical:</u> Tan Bloch Super Jazz Shoes</dd>--}}
+{{--                                </dl>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                    <div class="row font-syne-side">--}}
+{{--                        <div class="col-sm d-flex justify-content-end">--}}
+{{--                            <img src="/images/apparel-4th-grade-plus.png" alt="" class="img-fluid" style="max-height: 500px;">--}}
+{{--                        </div>--}}
+{{--                        <div class="col-sm d-flex align-items-center">--}}
+{{--                            <div>--}}
+{{--                                <strong>4th+ Grade</strong>--}}
+{{--                                <dl>--}}
+{{--                                    <dt class="fw-normal">- Black leotard/dress</dt>--}}
+{{--                                    <dt class="fw-normal">- Pink tights</dt>--}}
+{{--                                    <dt class="fw-normal">- Appropriate Class Shoes</dt>--}}
+{{--                                    <dd>-   <u>Acro:</u> Bare feet</dd>--}}
+{{--                                    <dd>-   <u>Hip Hop:</u> Icon Dance Sneaker</dd>--}}
+{{--                                    <dd>-   <u>Tap:</u> Black Capezio Tap Shoes</dd>--}}
+{{--                                    <dd>-   <u>Ballet:</u> Pink Revolution Stretch Ballet Shoes</dd>--}}
+{{--                                    <dd>-   <u>Jazz/Lyrical:</u> Tan Bloch Super Jazz Shoes</dd>--}}
+{{--                                </dl>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                    <h4 class="text-center font-staat-side">Male Students</h4>--}}
+{{--                    <div class="row font-syne-side">--}}
+{{--                        <div class="col-sm d-flex justify-content-end">--}}
+{{--                            <img src="/images/uniform-5.png" alt="" class="img-fluid" style="max-height: 500px;">--}}
+{{--                        </div>--}}
+{{--                        <div class="col-sm d-flex align-items-center">--}}
+{{--                            <div>--}}
+{{--                                <dl>--}}
+{{--                                    <dt class="fw-normal">- White tank top</dt>--}}
+{{--                                    <dt class="fw-normal">- Black joggers</dt>--}}
+{{--                                    <dt class="fw-normal">- Appropriate Class Shoes</dt>--}}
+{{--                                    <dd>-   <u>Acro:</u> Bare feet</dd>--}}
+{{--                                    <dd>-   <u>Hip Hop:</u> Icon Dance Sneaker</dd>--}}
+{{--                                    <dd>-   <u>Ballet:</u> White Revolution Stretch Ballet Shoes</dd>--}}
+{{--                                    <dd>-   <u>Jazz:</u> Black Bloch Super Jazz Shoes</dd>--}}
+{{--                                    <dd>-   <u>Tap:</u></dd>--}}
+{{--                                    <dd style="padding-left: 15px;">&middot;    Ages 2-4 - Black Revolution Oxford Tap Shoes</dd>--}}
+{{--                                    <dd style="padding-left: 15px;">&middot;    Kindergarten and Up - Black Capezio Oxford Tap Shoes</dd>--}}
+{{--                                </dl>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+
+            <div>
+                <img src="/images/uniform-26.jpg" class="img-fluid" alt="different dancers in uniform">
+            </div>
             </div>
         </div>
     </div>
