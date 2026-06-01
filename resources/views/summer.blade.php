7a6bec0f9b70cd17c7bf2615f7b0a429fb8b4989
denliedesign <customdenlie@gmail.com>
2026-06-01 11:35:40 -0500

    summer gfx update

--- resources/views/summer.blade.php
+++ resources/views/summer.blade.php
@@ -15,12 +15,12 @@
             <h2 class="text-center text-uppercase font-staat-side">Summer 2026</h2>
             <div class="container">
                 <div>
-                    <div class="d-flex justify-content-center my-5">
-                        <a href="/images/5-7-26-summer.pdf" target="_blank">
-                            <div class="btn btn-danger shadow">Download PDF</div>
-                        </a>
-                    </div>
-                    <div><img src="/images/5-7-26-summer.jpg" class="img-fluid" alt="summer graphic"></div>
+{{--                    <div class="d-flex justify-content-center my-5">--}}
+{{--                        <a href="/images/5-7-26-summer.pdf" target="_blank">--}}
+{{--                            <div class="btn btn-danger shadow">Download PDF</div>--}}
+{{--                        </a>--}}
+{{--                    </div>--}}
+                    <div><img src="/images/summer-6-1-26.jpg" class="img-fluid" alt="summer graphic"></div>
                     <div><img src="/images/5-7-26-summer-2.jpg" class="img-fluid" alt="summer graphic"></div>
                 </div>
                 <script type="text/javascript" src="https://app.jackrabbitclass.com/jr3.0/Openings/OpeningsJS?OrgID=383292&Session=Summer&hidecols=Gender,Ages,Session,Openings&sort=Class"></script>
